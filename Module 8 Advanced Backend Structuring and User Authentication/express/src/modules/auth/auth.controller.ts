efbfe07d286d98ea967cca2aacf7f647e94cfe1e
Chinmoy Sarkar <chinmoy4907@gmail.com>
2026-05-23 22:21:22 +0600

    Fix error handling in loginUser: ensure error message is included in response

--- Module 8 Advanced Backend Structuring and User Authentication/express/src/modules/auth/auth.controller.ts
+++ Module 8 Advanced Backend Structuring and User Authentication/express/src/modules/auth/auth.controller.ts
@@ -11,6 +11,7 @@ const loginUser = async (req: Request, res: Response) => {
   } catch (error: any) {
     res.status(500).json({
       success: false,
+      
       message: error.message,
       error: error,
     });
