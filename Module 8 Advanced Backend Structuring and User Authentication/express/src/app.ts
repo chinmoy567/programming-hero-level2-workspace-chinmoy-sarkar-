dd01ee7f74a0574291cf2f4b507f1410cc443ecd
Chinmoy Sarkar <chinmoy4907@gmail.com>
2026-05-23 21:06:48 +0600

    Implement authentication module: add login functionality with JWT token generation and update configuration for secret key

--- Module 8 Advanced Backend Structuring and User Authentication/express/src/app.ts
+++ Module 8 Advanced Backend Structuring and User Authentication/express/src/app.ts
@@ -1,7 +1,7 @@
-import express, { type Request, type Response } from "express";
-
+import express from "express";
 import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.routes";
+import { authRoute } from "./modules/auth/auth.route";
 
 const app = express();
 app.use(express.json());
@@ -10,5 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoute);
 app.use("/api/profile", profileRoute);
+app.use("/api/auth", authRoute);
 
 export default app;
