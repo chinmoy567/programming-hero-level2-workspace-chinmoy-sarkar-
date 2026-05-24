2da4822e550cde8261c71a096754b537f7d418e5
Chinmoy Sarkar <chinmoy4907@gmail.com>
2026-05-24 19:09:51 +0600

    Refactor authentication service to improve login and token generation logic

--- Module 9 Express Middleware, JWT  and Role-Based Authorization/express/src/modules/auth/auth.service.ts
+++ Module 9 Express Middleware, JWT  and Role-Based Authorization/express/src/modules/auth/auth.service.ts
@@ -3,29 +3,32 @@ import { pool } from "../../db";
 import jwt from "jsonwebtoken";
 import config from "../../config";
 
-// Function to handle user login and generate JWT token
+// Login User
 const loginUserIntoDB = async (payload: {
   email: string;
   password: string;
 }) => {
   const { email, password } = payload;
-  // Check if user exists
-  const userData = await pool.query(`SELECT * FROM users WHERE email=$1`, [
+
+  // Check user exists
+  const userData = await pool.query(`SELECT * FROM users WHERE email = $1`, [
     email,
   ]);
-  const user = userData.rows[0];
-  console.log(user);
 
   if (userData.rows.length === 0) {
     throw new Error("User not found");
   }
-  // Check if password matches
-  const matchpasword = await bcrypt.compare(password, user.password);
-  if (!matchpasword) {
+
+  const user = userData.rows[0];
+
+  // Check password
+  const matchPassword = await bcrypt.compare(password, user.password);
+
+  if (!matchPassword) {
     throw new Error("Invalid password");
   }
 
-  //generate token
+  // JWT payload
   const jwtPayload = {
     id: user.id,
     email: user.email,
@@ -34,46 +37,51 @@ const loginUserIntoDB = async (payload: {
     is_active: user.is_active,
   };
 
-  //access token with 365 days expiry
+  // Access Token
   const accessToken = jwt.sign(jwtPayload, config.secret_key as string, {
-    expiresIn: "365d",
+    expiresIn: "1d",
   });
 
-  //refresh token with 365 days expiry
+  // Refresh Token
   const refreshToken = jwt.sign(jwtPayload, config.secret_key as string, {
     expiresIn: "365d",
   });
 
-  return { accessToken, refreshToken };
+  return {
+    accessToken,
+    refreshToken,
+  };
 };
 
-// Function to verify refresh token and generate new tokens
+// Generate New Access Token
 const generateRefreshToken = async (token: string) => {
   if (!token) {
     throw new Error("Unauthorized");
   }
+
+  // Verify refresh token
   const decoded = jwt.verify(
     token,
     config.secret_key as string,
   ) as jwt.JwtPayload;
 
-  //fetch user from database using email from decoded token
-  const userData = await pool.query(
-    "SELECT * FROM users WHERE email = $1",
-    [decoded.email],
-  );
-  const user = userData.rows[0];
-  //if user is not found, return 401 Unauthorized
-  if (userData.rowCount === 0) { 
+  // Find user
+  const userData = await pool.query("SELECT * FROM users WHERE email = $1", [
+    decoded.email,
+  ]);
+
+  if (userData.rowCount === 0) {
     throw new Error("User not found");
   }
-  
-  //if user is not active, return 403 Forbidden
-  if (!user?.is_active) {
+
+  const user = userData.rows[0];
+
+  // Check active
+  if (!user.is_active) {
     throw new Error("User is not active");
   }
 
-  // Generate new token payloads
+  // New payload
   const jwtPayload = {
     id: user.id,
     email: user.email,
@@ -82,18 +90,115 @@ const generateRefreshToken = async (token: string) => {
     is_active: user.is_active,
   };
 
+  // New access token
   const accessToken = jwt.sign(jwtPayload, config.secret_key as string, {
-    expiresIn: "365d",
+    expiresIn: "1d",
   });
 
-  const refreshToken = jwt.sign(jwtPayload, config.secret_key as string, {
-    expiresIn: "365d",
-  });
-
-  return { accessToken, refreshToken };
+  return {
+    accessToken,
+  };
 };
 
 export const authService = {
   loginUserIntoDB,
   generateRefreshToken,
 };
+
+// import bcrypt from "bcryptjs";
+// import { pool } from "../../db";
+// import jwt from "jsonwebtoken";
+// import config from "../../config";
+
+// // Function to handle user login and generate JWT token
+// const loginUserIntoDB = async (payload: {
+//   email: string;
+//   password: string;
+// }) => {
+//   const { email, password } = payload;
+//   // Check if user exists
+//   const userData = await pool.query(`SELECT * FROM users WHERE email=$1`, [
+//     email,
+//   ]);
+//   const user = userData.rows[0];
+//   console.log(user);
+
+//   if (userData.rows.length === 0) {
+//     throw new Error("User not found");
+//   }
+//   // Check if password matches
+//   const matchpasword = await bcrypt.compare(password, user.password);
+//   if (!matchpasword) {
+//     throw new Error("Invalid password");
+//   }
+
+//   //generate token
+//   const jwtPayload = {
+//     id: user.id,
+//     email: user.email,
+//     name: user.name,
+//     role: user.role,
+//     is_active: user.is_active,
+//   };
+
+//   //access token with 365 days expiry
+//   const accessToken = jwt.sign(jwtPayload, config.secret_key as string, {
+//     expiresIn: "365d",
+//   });
+
+//     //refresh token with 365 days expiry
+//   const refreshToken = jwt.sign(jwtPayload, config.secret_key as string, {
+//     expiresIn: "365d",
+//   });
+
+//   const generateRefreshToken = async (token: string) => {
+
+//   if (!token) {
+//     throw new Error("Unauthorized");
+//   }
+
+//   const decoded = jwt.verify(
+//     token,
+//     config.secret_key as string,
+//   ) as jwt.JwtPayload;
+
+//   const userData = await pool.query(
+//     "SELECT * FROM users WHERE email = $1",
+//     [decoded.email],
+//   );
+
+//   const user = userData.rows[0];
+
+//   if (userData.rowCount === 0) {
+//     throw new Error("User not found");
+//   }
+
+//   if (!user?.is_active) {
+//     throw new Error("User is not active");
+//   }
+
+//   const jwtPayload = {
+//     id: user.id,
+//     email: user.email,
+//     name: user.name,
+//     role: user.role,
+//     is_active: user.is_active,
+//   };
+
+//   // create NEW access token
+//   const accessToken = jwt.sign(
+//     jwtPayload,
+//     config.secret_key as string,
+//     {
+//       expiresIn: "1d",
+//     }
+//   );
+
+//   return {
+//     accessToken,
+//   };
+// };
+
+// export const authService = {
+//   loginUserIntoDB,generateRefreshToken
+// };
