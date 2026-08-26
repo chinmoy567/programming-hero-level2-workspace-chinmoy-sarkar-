821b9ec25df7444fc021441cf8ec7e48e23a9341
Chinmoy Sarkar <chinmoy4907@gmail.com>
2026-08-26 14:14:32 +0600

    Implementing Jwt For Access Token And Refresh Token

--- Module 21 JWT Authentication and  Authorization Middleware/src/modules/auth/auth.service.ts
+++ Module 21 JWT Authentication and  Authorization Middleware/src/modules/auth/auth.service.ts
@@ -1,22 +1,50 @@
+import config from "../../config";
 import { prisma } from "../../lib/prisma";
+import { jwtUtils } from "../../utils/jwt";
 import { ILoginUser } from "./auth.interface";
 import bcrypt from "bcrypt";
 
+const loginUser = async (payload: ILoginUser) => {
+  const { email, password } = payload;
 
-const loginUser =async (payload: ILoginUser)=> {
-const {email,password} = payload;
-    
-//find user by email
-const user =await prisma.user.findUniqueOrThrow({
-    where:{email}
-})
-//check password
-const isPasswordMatched = await bcrypt.compare(password, user.password);
-if(!isPasswordMatched){
-    throw new Error("Password is incorrect");      
+  //find user by email
+  const user = await prisma.user.findUniqueOrThrow({
+    where: { email },
+  });
+  //check password
+  const isPasswordMatched = await bcrypt.compare(password, user.password);
+  if (!isPasswordMatched) {
+    throw new Error("Password is incorrect");
+  }
+
+  //create jwt payload
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+
+  //create acess token
+  const accessToken = jwtUtils.createToken(
+    jwtPayload,
+    config.JWT_ACCESS_SECRET,
+    config.JWT_ACCESS_EXPIRES_IN,
+  );
+
+  //create refresh token
+  const refreshToken = jwtUtils.createToken(
+    jwtPayload,
+    config.JWT_REFRESH_SECRET,
+    config.JWT_REFRESH_EXPIRES_IN,
+  );
+
+
+  return {
+    accessToken,
+    refreshToken,
+  };
 };
-return user;
-}
 export const authService = {
-    loginUser
-}
+  loginUser,
+};
